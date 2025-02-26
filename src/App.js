d7406bcce746aa3f1f5e376ea90605eacec0bcf7
Mithun-18 <mithunpoojary180@gmail.com>
2025-02-26 10:34:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
+import Heading from "./components/Heading";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <div className="flex justify-center">
+        <div className="w-fit">
+          <AddContact />
+          <ContactList />
+        </div>
+      </div>
     </div>
   );
 }
